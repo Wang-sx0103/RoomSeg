e400fbfd446220cc378f0677888f575f7faeea54
Alex <AlexWang0103@outlook.com>
2023-12-23 18:30:21 +0800

    Delete useless comments

--- MainWidget/include/Cloud.hpp
+++ MainWidget/include/Cloud.hpp
@@ -10,33 +10,6 @@
 
 #include <string>
 #include <memory>
-#include <pcl/point_cloud.h>
-
-//class CloudBase
-//{
-//public:
-//    explicit CloudBase(const std::string& name): mNameID(name), mStatus(true) {}
-//    const std::string getName() const
-//    {
-//        return this->mNameID;
-//    }
-//
-//    void setShowStatus(const bool status)
-//    {
-//        this->mStatus = status;
-//    }
-//    const bool getShowStatus() const
-//    {
-//        return this->mStatus;
-//    }
-//    virtual void setCloudPtr() const = 0;
-//    virtual void getCloudPtr() const = 0;
-//    virtual ~CloudBase() {}
-//
-//private:
-//    std::string mNameID;
-//    bool mStatus;
-//};
 
 template<typename T>
 class Cloud
